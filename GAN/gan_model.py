5df5b286dceedd5695f3a6005b4b1f39446dc8ac
chrimerss <chrimerss@gmail.com>
2019-05-27 17:20:47 +0800

    update lagrangian

--- GAN/gan_model.py
+++ GAN/gan_model.py
@@ -1,2 +1,19 @@
 import torch.nn as nn
 import torch
+
+
+class Generator(nn.Module):
+	def __init__(self,use_gpu=True):
+		pass
+
+	def forward(self, x):
+		#input data shape (bsize, tsize, channel, m, n)
+		pass
+
+
+class Descrimitor(nn.Module):
+	def __init__(self, use_gpu=True):
+		pass
+
+	def forward(self,x):
+		#input data shape (bsize, tsize, channel, m, n)
